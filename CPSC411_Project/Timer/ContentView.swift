756d1e802f9191bb535ba5a367427f61c268c9d8
Nalluh <120067040+Nalluh@users.noreply.github.com>
2023-11-17 13:26:06 -0800

    UI Changes by j.spark1

--- CPSC411_Project/Timer/ContentView.swift
+++ CPSC411_Project/Timer/ContentView.swift
@@ -1,50 +1,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("nightMode") var nightMode = false
     @State  var name: String = ""
     @State  var goal: String = ""
     @State  var isSignedIn: Bool = false
-
+    @State  var isFormValid: Bool = false
+    
     var body: some View {
         NavigationView {
             VStack {
                 Text("Welcome to StudyBuddy!")
                     .padding(10)
                     .font(.custom("AppleSDGothicNeo-Bold", size: 26))
-                    .border(Color.black)
+                    .border(nightMode ? Color.white : Color.black)
                     .cornerRadius(2)
-                    .background(Color.white)
+                    .background(nightMode ? Color.black : Color.white)
                     .padding(80)
                 Text("Please fill infomation below")
-                TextField("Name", text: $name)
+                InputFieldView(data: $name, title: "Name")
+                    .onChange(of: name) { newValue in
+                            validateForm()
+                    }
                     .padding()
-                TextField("Goal", text: $goal)
+                InputFieldView(data: $goal, title: "Goal")
+                    .onChange(of: goal) { newValue in
+                            validateForm()
+                    }
                     .padding()
-
-            
-
+                
+                
+                
                 Button(action: {
                     // Additional logic can be added here to validate data before navigating
                     isSignedIn = true
-                }) {
+                }) { // Submit Button will stay gray until credentials are filled and change color to blue
                     Text("Submit")
                         .padding(12)
-                        .background(Color.blue)
+                        .fontWeight(.heavy)
+                        .font(.title3)
+                        .frame(maxWidth: .infinity)
+                        .background(isFormValid ? LinearGradient(gradient: Gradient(colors: [Color.teal, Color.blue]), startPoint: .leading, endPoint: .trailing) : LinearGradient(gradient: Gradient(colors: [Color.gray, Color.gray]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
                         .foregroundColor(Color.white)
-                        .cornerRadius(15)
+                        .cornerRadius(35)
                 }
+                .disabled(!isFormValid)
                 .padding(12)
                 NavigationLink(destination:  MenuOptions(name:name,goal:goal), isActive: $isSignedIn) {
-                   
+                    
                 }
                 .hidden()
             }
+            .preferredColorScheme(nightMode ? .dark : .light)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        nightMode.toggle()
+                    }) {
+                        Image(systemName: nightMode ? "moon.stars.fill" : "sun.max.fill")
+                            .imageScale(.large)
+                    }
+                }
+            }
             .padding()
-         
+            
         }
     }
+    
+    func validateForm() {
+        isFormValid = !name.isEmpty && !goal.isEmpty
+    }
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
--- CPSC411_Project/Timer/Views.swift
+++ CPSC411_Project/Timer/Views.swift
@@ -339,6 +339,35 @@ struct TimerView: View {
     }
 }
 
+struct InputFieldView: View {
+    @AppStorage("nightMode") var nightMode = false
+    @Binding var data: String
+    var title: String?
+    
+    var body: some View {
+        ZStack {
+            TextField("", text: $data)
+              .padding(.horizontal, 10)
+              .frame(height: 42)
+              .overlay(
+                RoundedRectangle(cornerSize: CGSize(width: 4, height: 4))
+                    .stroke(Color.gray, lineWidth: 1)
+              )
+            HStack {                    // HStack for the text
+              Text(title ?? "Input")
+                .font(.headline)
+                .fontWeight(.thin)      // making the text small
+                .foregroundColor(Color.gray)    // and gray
+                .multilineTextAlignment(.leading)
+                .padding(4)
+                .background(nightMode ? Color.black : Color.white)     // adding some white background
+              Spacer()                  // pushing the text to the left
+            }
+            .padding(.leading, 8)
+            .offset(CGSize(width: 0, height: -20))  // pushign the text up to overlay the border of the input field
+          }.padding(4)
+    }
+}
 struct Flashcards: View {
     @State private var newFlashcardTitle: String = ""
     @State private var isCreatingNewFlashcard: Bool = false
